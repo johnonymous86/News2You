3deca1b4e10fd69e44be42f6fb40509fa122d3e9
John Caudill <jcaudill86@gmail.com>
2026-04-29 14:43:25 -0400

    mongoose user preference

---
+++ db/models/preference.js
@@ -0,0 +1,24 @@
+import { Schema, model, models } from 'mongoose'
+
+const PreferenceSchema = new Schema({
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: 'Users',
+    required: true,
+    unique: true
+  },
+  topics: {
+    type: [String],
+    default: []
+  },
+  keywords: {
+    type: [String],
+    default: []
+  },
+  sources: {
+    type: [String],
+    default: []
+  }
+}, { timestamps: true })
+
+export default models.Preference || model('Preference', PreferenceSchema)
