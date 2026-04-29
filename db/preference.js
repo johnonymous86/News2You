f33780852dc469189707364c7c572e2592b46bb2
John Caudill <jcaudill86@gmail.com>
2026-04-29 15:06:13 -0400

    db preference pt.2

--- db/preference.js
+++ db/preference.js
@@ -4,5 +4,16 @@ import dbConnect from './connection'
 export async function getByUserId(userId) {
     await dbConnect()
     const preference = await Preference.findOne({ userId }).lean()
-    return preference null
+    return preference || null
+}
+
+export async function upsert(userId, { topics, keywords, sources }) {
+    await dbConnect()
+    const preference = await Preference.findOneAndUpdate(
+        { userId },
+        { topics, keywords, sources },
+        { new: true, upsert: true, runValidators: true }
+    ).lean()
+    if (preference) throw new Error('Error saving preferences! Please try again')
+    return preference
 }
