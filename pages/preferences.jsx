74e9cb7d625f3be37df813234ed70baf47e274d7
John Caudill <jcaudill86@gmail.com>
2026-04-29 16:33:17 -0400

    preferences selector setup

--- pages/preferences.jsx
+++ pages/preferences.jsx
@@ -1,5 +1,48 @@
 import { useState } from "react";
-import { withIronSessionApiRoute } from "iron-session/next";
+import { withIronSessionApiRoute, withIronSessionSsr } from "iron-session/next";
 import sessionsOptions from "../config/session";
 import Header from "../components/header";
 
+export const getServerSideProps = withIronSessionSsr(
+    async function getServerSideProps({ req }) {
+      const user = req.session.user;
+      if (!user) {
+        return {
+          redirect: { destination: "/login", permanent: false },
+        };
+      }
+      return {
+        props: { user, isLoggedIn: true },
+      };
+    },
+    sessionOptions
+  );
+
+  const AVAILABLE_TOPICS = [
+    "test",
+  ]
+
+
+  return (
+    <div>
+        <Header isLoggedIn={isLoggedIn} username={user.username} />
+
+        <main>
+            <h1>Your Preferences</h1>
+
+            <section>
+                <p>TOPICS</p>
+                {AVAILABLE_TOPICS.map((topic) =>(
+                    <label key={topic}>
+                        <input
+                        type="checkbox"
+                        checked={topics.includes(topic)}
+                        onChange={() => toggleTopic(topic)}
+                        />
+                        {topic}
+                        </label>
+                ))}
+            </section>
+        </main>
+    </div>
+  )
