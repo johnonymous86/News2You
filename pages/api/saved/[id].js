cd038b71d452da046367e35f668888707aa69334
John Caudill <jcaudill86@gmail.com>
2026-04-29 22:02:19 -0400

    API routing setup

--- pages/api/saved/[id].js
+++ pages/api/saved/[id].js
@@ -8,5 +8,14 @@ export default withIronSessionApiRoute(
         if (!user) return res.status(401).json({ error: "You're not logged in."})
         
         switch (req.method) {}
-    }
-)
+        switch (req.method) {
+            case 'PUT':
+              return updateArticle(req, res, user)
+            case 'DELETE':
+              return deleteArticle(req, res, user)
+            default:
+              return res.status(405).end()
+          }
+        },
+        sessionOptions
+      )
