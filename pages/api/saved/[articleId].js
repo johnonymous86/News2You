8524dedd09b2a56998375e0d728ba97801052425
John Caudill <jcaudill86@gmail.com>
2026-04-30 09:21:47 -0400

    update articleId.js

--- pages/api/saved/[articleId].js
+++ pages/api/saved/[articleId].js
@@ -1,6 +1,6 @@
 import { withIronSessionApiRoute } from 'iron-session/next'
 import sessionOptions from '../../../../config/session'
-import { markAsRead, remove } from '../../../../db/savedArticle'
+import { updateArticle, deleteArticle } from '../../../../controllers/savedArticle'
 
 export default withIronSessionApiRoute(
   async function handler(req, res) {
@@ -18,25 +18,3 @@ export default withIronSessionApiRoute(
   },
   sessionOptions
 )
-
-async function updateArticle(req, res, user) {
-  try {
-    const { articleId } = req.query
-    const article = await markAsRead(articleId, user._id)
-    res.status(200).json(article)
-  } catch (err) {
-    console.error('PUT /api/saved/[articleId] error:', err)
-    res.status(500).json({ error: 'Internal Server Error' })
-  }
-}
-
-async function deleteArticle(req, res, user) {
-  try {
-    const { articleId } = req.query
-    await remove(articleId, user._id)
-    res.status(200).json({ message: 'Article removed' })
-  } catch (err) {
-    console.error('DELETE /api/saved/[articleId] error:', err)
-    res.status(500).json({ error: 'Internal Server Error' })
-  }
-}
