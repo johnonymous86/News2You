e8828a40ff2f5f4840f64c1dc3b17993de1cb8e3
John Caudill <jcaudill86@gmail.com>
2026-05-01 18:49:22 -0400

    header and card styling

--- components/nav/index.jsx
+++ components/nav/index.jsx
@@ -4,8 +4,8 @@ import styles from "./style.module.css";
 export default function Nav() {
   return (
     <nav className={styles.nav}>
-      <Link href="/feed">Feed | </Link>
-      <Link href="/preferences">Preferences | </Link>
+      <Link href="/feed">Feed</Link>
+      <Link href="/preferences">Preferences</Link>
       <Link href="/saved">Saved</Link>
     </nav>
   );
