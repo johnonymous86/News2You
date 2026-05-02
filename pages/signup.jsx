2b7047dda88fa6b8ec5f8e6dc09dfafd0a393620
John Caudill <jcaudill86@gmail.com>
2026-05-01 21:32:19 -0400

    final push

--- pages/signup.jsx
+++ pages/signup.jsx
@@ -39,7 +39,7 @@ export default function Signup(props) {
         },
         body: JSON.stringify({ username, password }),
       });
-      if (res.status === 200) return router.push("/dashboard");
+      if (res.status === 200) return router.push("/preferences");
       const { error: message } = await res.json();
       setError(message);
     } catch (err) {
